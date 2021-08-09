2e6c6b0f8cb3eeed3e600dcefb9fec558e7d8a78
Saikrishna-8 <221710307008@gitam.in>
2021-08-09 11:32:41 +0530

    Meeting rooms

--- DS_Algo/src/arrays/MeetingRooms.java
+++ DS_Algo/src/arrays/MeetingRooms.java
@@ -1,9 +1,63 @@
 package arrays;
 
+import java.util.*;
+
 public class MeetingRooms {
 
 	public MeetingRooms() {
 		// TODO Auto-generated constructor stub
 	}
+	public static void main(String args[])
+	{
+		
+	}
+	
 
+}
+
+
+
+// https://leetcode.com/problems/meeting-rooms-ii/
+// problem description Source
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        
+        Arrays.sort(intervals,new Comparator<int[]>() {
+			
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				return o1[0]-o2[0];
+			}
+		}
+				
+		);
+ 	     return MinimumRooms(intervals);	
+        
+    }
+   public static int MinimumRooms(int[][] intervals)
+	{
+		Queue<Integer> pq=new PriorityQueue<Integer>(intervals.length,new Comparator<Integer>(){
+			
+			 public int compare(Integer a, Integer b) {
+	                return a - b;
+	              }
+		});
+		pq.add(intervals[0][1]);
+		
+		for(int i=1;i<intervals.length;i++)
+		{
+			if(intervals[i][0]>=pq.peek())
+			{
+				pq.poll();
+				pq.add(intervals[i][1]);
+			}
+			else
+			{
+				pq.add(intervals[i][1]);
+			}
+		}
+		return pq.size();
+	}
 }
--- DS_Algo/src/arrays/MeetingRooms2.java
+++ DS_Algo/src/arrays/MeetingRooms2.java
@@ -1,4 +1,5 @@
 package arrays;
+import java.util.*;
 
 public class MeetingRooms2 {
 
@@ -6,4 +7,58 @@ public class MeetingRooms2 {
 		// TODO Auto-generated constructor stub
 	}
 
+	public static void main(String args[])
+	{
+		
+	}
+}
+
+
+
+/*https://leetcode.com/problems/meeting-rooms-ii/
+ * 
+ * problem description Source       
+ * 
+ * Approach 2
+ */
+
+
+ 
+class Solution1 {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] start=new int[intervals.length];
+		int[] end=new int[intervals.length];
+		
+		for(int i=0;i<intervals.length;i++)
+		{
+			start[i]=intervals[i][0];
+		}
+		for(int i=0;i<intervals.length;i++)
+		{
+			end[i]=intervals[i][1];
+		}
+		return MinimumRooms(start,end);
+        
+    }
+    public int MinimumRooms(int[] start,int[] end)
+	{
+		Arrays.sort(start);
+		Arrays.sort(end);
+		
+		int count=1;
+		int i=1,j=0;
+		while(i<start.length)
+		{
+			if(start[i]>=end[j])
+			{
+				i++;
+				j++;
+			}
+			else {
+				i++;
+				count++;
+			}
+		}
+		return count;
+	}
 }
